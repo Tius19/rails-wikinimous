a9a9ee0186c1bc3e0d61c8050f18c23b24137a52
Leo DHotman <dhotman19@yahoo.com>
2023-05-23 16:49:44 +0400

    wiki

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+Article.destroy_all
+i = 0
+10.times do
+  Article.create(
+    title: Faker::GreekPhilosophers.name,
+    content: Faker::GreekPhilosophers.quote
+  )
+  puts "creating #{i += 1}"
+end
